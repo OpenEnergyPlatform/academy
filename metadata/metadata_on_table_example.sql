441c6922f6bba15caeb33c23dd7e0b3a9bf83d12
Ludee <Ludwig.Huelk@rl-institut.de>
2018-06-12 14:17:39 +0200

    add second example source (OSM) and fix minor bugs

--- metadata/metadata_on_table_example.sql
+++ metadata/metadata_on_table_example.sql
@@ -5,7 +5,7 @@ A further description can be found on http://wiki.openmod-initiative.org/wiki/Da
 Feedback for an update is collected in this issue: https://github.com/OpenEnergyPlatform/organisation/issues/10
 
 __copyright__   = "© Reiner Lemoine Institut"
-__license__     = "Creative Commons Zero v1.0 Universal"
+__license__     = "Creative Commons Zero v1.0 Universal (CC0-1.0)"
 __url__         = "https://creativecommons.org/publicdomain/zero/1.0/"
 __author__      = "Ludee"
 __contains__    = "http://stackoverflow.com/questions/383692/what-is-json-and-why-would-i-use-it","https://specs.frictionlessdata.io/data-package/"
@@ -44,8 +44,8 @@ COMMENT ON TABLE model_draft.test_table IS '{
         "end": "2017-12-31",
         "resolution": "hour"},
     "sources": [
-        {"name": "eGo dataprocessing", "description": "", "url": "https://github.com/openego/data_processing", "license": "GNU Affero General Public License Version 3 (AGPL-3.0)", "copyright": "© Reiner Lemoine Institut"},
-        {"name": "", "description": "", "url": "", "license": "", "copyright": ""} ],
+        {"name": "OpenEnergyPlatform Metadata Example", "description": "Metadata description", "url": "https://github.com/OpenEnergyPlatform", "license": "Creative Commons Zero v1.0 Universal (CC0-1.0)", "copyright": "© Reiner Lemoine Institut"},
+        {"name": "OpenStreetMap", "description": "A collaborative project to create a free editable map of the world", "url": "https://www.openstreetmap.org/", "license": "ODbL-1.0", "copyright": "© OpenStreetMap contributors"} ],
     "license": 
         {"id": "ODbL-1.0",
         "name": "Open Data Commons Open Database License 1.0",
@@ -54,21 +54,21 @@ COMMENT ON TABLE model_draft.test_table IS '{
         "instruction": "You are free: To Share, To Create, To Adapt; As long as you: Attribute, Share-Alike, Keep open!",
         "copyright": "© Reiner Lemoine Institut"},
     "contributors": [
-        {"name": "Ludee", "email": "", "date": "2016-06-16", "comment": "Create metadata"},
-        {"name": "Ludee", "email": "", "date": "2016-11-22", "comment": "Update metadata"},
-        {"name": "Ludee", "email": "", "date": "2016-11-22", "comment": "Update header and license"},
-        {"name": "Ludee", "email": "", "date": "2017-03-16", "comment": "Add license to source"},
-        {"name": "Ludee", "email": "", "date": "2017-03-28", "comment": "Add copyright to source and license"},
-        {"name": "Ludee", "email": "", "date": "2017-05-30", "comment": "Update metadata to version 1.3"},
-        {"name": "Ludee", "email": "", "date": "2017-06-26", "comment": "Update metadata version 1.3: move reference_date into temporal and remove some array"} ],
+        {"name": "Ludee", "email": "none", "date": "2016-06-16", "comment": "Create metadata"},
+        {"name": "Ludee", "email": "none", "date": "2016-11-22", "comment": "Update metadata"},
+        {"name": "Ludee", "email": "none", "date": "2016-11-22", "comment": "Update header and license"},
+        {"name": "Ludee", "email": "none", "date": "2017-03-16", "comment": "Add license to source"},
+        {"name": "Ludee", "email": "none", "date": "2017-03-28", "comment": "Add copyright to source and license"},
+        {"name": "Ludee", "email": "none", "date": "2017-05-30", "comment": "Update metadata to version 1.3"},
+        {"name": "Ludee", "email": "none", "date": "2017-06-26", "comment": "Update metadata version 1.3: move reference_date into temporal and remove some array"} ],
     "resources": [
         {"name": "model_draft.test_table",
         "format": "PostgreSQL",
         "fields": [
-            {"name": "id", "description": "Unique identifier", "unit": ""},
-            {"name": "year", "description": "Reference year", "unit": ""},
+            {"name": "id", "description": "Unique identifier", "unit": "none"},
+            {"name": "year", "description": "Reference year", "unit": "none"},
             {"name": "value", "description": "Example value", "unit": "MW"},
-            {"name": "geom", "description": "Geometry", "unit": ""} ] } ],
+            {"name": "geom", "description": "Geometry", "unit": "none"} ] } ],
     "metadata_version": "1.3"}';
 
 -- select description
