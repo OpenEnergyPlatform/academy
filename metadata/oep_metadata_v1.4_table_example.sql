a3ecd3a02f61db9d476bc27e1c090476c1cf85c9
Ludee <Ludwig.Huelk@rl-institut.de>
2018-07-19 17:32:35 +0200

    create first version for new metadata version 1.4 https://github.com/OpenEnergyPlatform/organisation/issues/10

---
+++ metadata/oep_metadata_v1.4_table_example.sql
@@ -0,0 +1,137 @@
+/*
+This script provides an SQL example of the metadata documentation
+A definition of metadata can be found in the openmod glossary http://wiki.openmod-initiative.org/wiki/Metadata
+A further description can be found on http://wiki.openmod-initiative.org/wiki/DatabaseRules
+Feedback for an update is collected in this issue: https://github.com/OpenEnergyPlatform/organisation/issues/10
+
+__url__         = "https://github.com/OpenEnergyPlatform/examples/tree/master/metadata"
+__copyright__   = "© Reiner Lemoine Institut"
+__license__     = "Creative Commons Zero v1.0 Universal (CC0-1.0)"
+__license_url__ = "https://creativecommons.org/publicdomain/zero/1.0/"
+__author__      = "Ludee"
+__contains__    = "http://stackoverflow.com/questions/383692/what-is-json-and-why-would-i-use-it","https://specs.frictionlessdata.io/data-package/"
+
+-- metadata description
+-- metadata template
+
+Additional information:
+- Dates and time must follow the ISO8601 (JJJJ-MM-TT; 1 a; 1 h)
+- Use a space between Numbers and units (100 m)
+- Languages must follow the IETF (BCP47) format (en-US; de-DE)
+- If not applicable use "none"
+*/
+
+-- test table
+DROP TABLE IF EXISTS    model_draft.test_table_v14;
+CREATE TABLE            model_draft.test_table_v14 (
+    id      serial,
+    year    integer,
+    value   double precision,
+    geom    geometry(Point, 4326),
+    CONSTRAINT table_pkey PRIMARY KEY (id) );
+
+
+-- metadata description
+COMMENT ON TABLE model_draft.test_table_v14 IS '{
+    "title": "Good example title",
+    "description": "example metadata for example data",
+    "language": [ "en-GB", "en-US", "de-DE", "fr-FR" ],
+    "keywords": [ "example", "template", "test" ],
+    "spatial": 
+        {"location": "none",
+        "extent": "europe",
+        "resolution": "100 m"},
+    "temporal": 
+        {"reference_date": "2016-01-01",
+        "start": "2017-01-01",
+        "end": "2017-12-31",
+        "resolution": "1 h"},
+    "sources": [
+        {"name": "OpenEnergyPlatform Metadata Example", "description": "Metadata description", "url": "https://github.com/OpenEnergyPlatform", "license": "Creative Commons Zero v1.0 Universal (CC0-1.0)", "copyright": "© Reiner Lemoine Institut"},
+        {"name": "OpenStreetMap", "description": "A collaborative project to create a free editable map of the world", "url": "https://www.openstreetmap.org/", "license": "ODbL-1.0", "copyright": "© OpenStreetMap contributors"} ],
+    "license": 
+        {"id": "ODbL-1.0",
+        "name": "Open Data Commons Open Database License 1.0",
+        "url": "https://opendatacommons.org/licenses/odbl/1.0/",
+        "instruction": "You are free: To Share, To Create, To Adapt; As long as you: Attribute, Share-Alike, Keep open!",
+        "copyright": "© Reiner Lemoine Institut"},
+    "contributors": [
+        {"name": "Ludee", "email": "none", "date": "2016-06-16", "comment": "Create metadata"},
+        {"name": "Ludee", "email": "none", "date": "2016-11-22", "comment": "Update metadata"},
+        {"name": "Ludee", "email": "none", "date": "2016-11-22", "comment": "Update header and license"},
+        {"name": "Ludee", "email": "none", "date": "2017-03-16", "comment": "Add license to source"},
+        {"name": "Ludee", "email": "none", "date": "2017-03-28", "comment": "Add copyright to source and license"},
+        {"name": "Ludee", "email": "none", "date": "2017-05-30", "comment": "Update metadata to version 1.3"},
+        {"name": "Ludee", "email": "none", "date": "2017-06-26", "comment": "Update metadata version 1.3: move reference_date into temporal and remove some array"} ],
+    "resources": [
+        {"name": "model_draft.test_table_v14",
+        "format": "PostgreSQL",
+        "encoding" : "UTF-8",
+        "fields": [
+            {"name": "id", "description": "Unique identifier", "type": "serial", "unit": "none"},
+            {"name": "year", "description": "Reference year", "type": "integer", "unit": "none"},
+            {"name": "value", "description": "Example value", "type": "double precision", "unit": "MW"},
+            {"name": "geom", "description": "Geometry", "type": "geometry(Point, 4326)", "unit": "none"} ] } ],
+        "_comment": {
+            "_metadata_version": "1.4" },
+            "_metadata_license": "Creative Commons Zero v1.0 Universal (CC0-1.0)",
+            "_metadata_license_url": "https://creativecommons.org/publicdomain/zero/1.0/" }';
+
+-- select description
+SELECT obj_description('model_draft.test_table_v14' ::regclass) ::json;
+
+-- select description
+SELECT obj_description('model_draft.test_table_v14' ::regclass) ::json;
+
+
+-- metadata template
+COMMENT ON TABLE model_draft.test_table_v14 IS '{
+    "title": "",
+    "description": "",
+    "language": [ "en-GB" ],
+    "keywords": [ "" ],
+    "spatial": 
+        {"location": "",
+        "extent": "",
+        "resolution": ""},
+    "temporal": 
+        {"reference_date": "",
+        "start": "",
+        "end": "",
+        "resolution": ""},
+    "sources": [
+        {"name": "", "description": "", "url": "", "license": "", "copyright": ""},
+        {"name": "", "description": "", "url": "", "license": "", "copyright": ""} ],
+    "license": 
+        {"id": "",
+        "name": "",
+        "url": "",
+        "instruction": "",
+        "copyright": ""},
+    "contributors": [
+        {"name": "", "email": "", "date": "", "comment": ""},
+        {"name": "", "email": "", "date": "", "comment": ""} ],
+    "resources": [
+        {"name": "",
+        "format": "PostgreSQL",
+        "encoding" : "UTF-8",
+        "fields": [
+            {"name": "id", "description": "Unique identifier", "type": "serial", "unit": "none"},
+            {"name": "year", "description": "Reference year", "type": "integer", "unit": "none"},
+            {"name": "value", "description": "Example value", "type": "double precision", "unit": "none"},
+            {"name": "geom", "description": "Geometry", "type": "geometry(Point, 4326)", "unit": "none"} ] } ],
+    "_comment": {
+        "_metadata_version": "1.4",
+        "_url": "https://github.com/OpenEnergyPlatform/examples/tree/master/metadata",
+        "_copyright": "© Reiner Lemoine Institut",
+        "_metadata_license": "Creative Commons Zero v1.0 Universal (CC0-1.0)",
+        "_metadata_license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
+        "_contains": " http://www.json.org/; http://stackoverflow.com/questions/383692/what-is-json-and-why-would-i-use-it",
+        "_additional_information": {
+            "_dates": "Dates and time must follow the ISO8601 (JJJJ-MM-TT)",
+            "_units": "Use a space between Numbers and units (100 m)",
+            "_languages": "Languages must follow the IETF (BCP47) format (en-US; de-DE)",
+            "_none": "If not applicable use 'none'"} } }';
+
+-- select description
+SELECT obj_description('model_draft.test_table_v14' ::regclass) ::json;
