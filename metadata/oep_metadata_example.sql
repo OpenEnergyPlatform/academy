2354f2ee393ba2dfc808f5cb8d6f41c38101fd80
Ludee <Ludwig.Huelk@rl-institut.de>
2019-07-09 13:32:29 +0200

    update metadata source licenses #58

--- metadata/oep_metadata_example.sql
+++ metadata/oep_metadata_example.sql
@@ -39,11 +39,11 @@ COMMENT ON TABLE model_draft.oep_metadata_table_example_v14 IS '
 "description": "example metadata for example data",
 "language": [ "en-GB", "en-US", "de-DE", "fr-FR" ],
 "keywords": [ "example", "template", "test" ],
-"publication_date": "2018-06-12",
+"publicationDate": "2018-06-12",
 "context": 
     {"homepage": "https://reiner-lemoine-institut.de/szenariendb/",
     "documentation": "https://github.com/OpenEnergyPlatform/organisation/wiki/metadata",
-    "source_code": "https://github.com/OpenEnergyPlatform/examples/tree/master/metadata",
+    "sourceCode": "https://github.com/OpenEnergyPlatform/examples/tree/master/metadata",
     "contact": "https://github.com/Ludee",
     "grantNo": "03ET4057",
     "fundingAgency": "Bundesministerium für Wirtschaft und Energie",
@@ -64,20 +64,28 @@ COMMENT ON TABLE model_draft.oep_metadata_table_example_v14 IS '
 "sources": [
     {"title": "OpenEnergyPlatform Metadata Example",
     "description": "Metadata description",
-    "url": "https://github.com/OpenEnergyPlatform",
-    "license": "Creative Commons Zero v1.0 Universal (CC0-1.0)", 
-    "copyright": "© Reiner Lemoine Institut"},
+    "path": "https://github.com/OpenEnergyPlatform", 
+    "licenses": [
+        {"name": "CC0-1.0",
+        "title": "Creative Commons Zero v1.0 Universal",
+        "path": "https://creativecommons.org/publicdomain/zero/1.0/legalcode",
+        "instruction": "You are free: To Share, To Create, To Adapt",
+        "attribution": "© Reiner Lemoine Institut"} ] },
     {"title": "OpenStreetMap",
     "description": "A collaborative project to create a free editable map of the world",
-    "url": "https://www.openstreetmap.org/",
-    "license": "ODbL-1.0",
-    "copyright": "© OpenStreetMap contributors"} ],
+    "path": "https://www.openstreetmap.org/",
+    "licenses": [
+        {"name": "ODbL-1.0",
+        "title": "Open Data Commons Open Database License 1.0",
+        "path": "https://opendatacommons.org/licenses/odbl/1.0/index.html",
+        "instruction": "You are free: To Share, To Create, To Adapt; As long as you: Attribute, Share-Alike, Keep open!",
+        "attribution": "© OpenStreetMap contributors"} ] } ],
 "licenses": [
-    {"id": "ODbL-1.0",
+    {"name": "ODbL-1.0",
     "title": "Open Data Commons Open Database License 1.0",
-    "url": "https://opendatacommons.org/licenses/odbl/1.0/",
+    "path": "https://opendatacommons.org/licenses/odbl/1.0/",
     "instruction": "You are free: To Share, To Create, To Adapt; As long as you: Attribute, Share-Alike, Keep open!",
-    "copyright": "© Reiner Lemoine Institut"}],
+    "attribution": "© Reiner Lemoine Institut © OpenStreetMap contributors"}],
 "contributors": [
     {"title": "Ludee", "email": null, "date": "2016-06-16", "object": "metadata", "comment": "Create metadata"},
     {"title": "Ludee", "email": null, "date": "2016-11-22", "object": "metadata", "comment": "Update metadata"},
@@ -85,14 +93,15 @@ COMMENT ON TABLE model_draft.oep_metadata_table_example_v14 IS '
     {"title": "Ludee", "email": null, "date": "2017-03-16", "object": "metadata", "comment": "Add license to source"},
     {"title": "Ludee", "email": null, "date": "2017-03-28", "object": "metadata", "comment": "Add copyright to source and license"},
     {"title": "Ludee", "email": null, "date": "2017-05-30", "object": "metadata", "comment": "Release metadata version 1.3"},
-    {"title": "Ludee", "email": null, "date": "2017-06-26", "object": "metadata", "comment": "Move reference_date into temporal and remove array"},
+    {"title": "Ludee", "email": null, "date": "2017-06-26", "object": "metadata", "comment": "Move referenceDate into temporal and remove array"},
     {"title": "Ludee", "email": null, "date": "2018-07-19", "object": "metadata", "comment": "Start metadata version 1.4"},
     {"title": "Ludee", "email": null, "date": "2018-07-26", "object": "data", "comment": "Rename table and files"},
     {"title": "Ludee", "email": null, "date": "2018-10-18", "object": "metadata", "comment": "Add contribution object"},
     {"title": "christian-rli", "email": null, "date": "2018-10-18", "object": "metadata", "comment": "Add datapackage compatibility"},
     {"title": "Ludee", "email": null, "date": "2018-11-02", "object": "metadata", "comment": "Release metadata version 1.4"},
-    {"title": "christian-rli", "email": null, "date": "2019-02-05", "object": "metadata", "comment": "Apply template structure to example."},
-    {"title": "Ludee", "email": null, "date": "2019-03-22", "object": "metadata", "comment": "Hotfix foreignKeys"} ],
+    {"title": "christian-rli", "email": null, "date": "2019-02-05", "object": "metadata", "comment": "Apply template structure to example"},
+    {"title": "Ludee", "email": null, "date": "2019-03-22", "object": "metadata", "comment": "Hotfix foreignKeys"},
+    {"title": "Ludee", "email": null, "date": "2019-07-09", "object": "metadata", "comment": "Release metadata version OEP-1.3.0"} ],
 "resources": [
     {"profile": "tabular-data-resource",
     "name": "model_draft.oep_metadata_table_example_v14",
@@ -113,16 +122,24 @@ COMMENT ON TABLE model_draft.oep_metadata_table_example_v14 IS '
                     "fields": ["year"] } } ] },
     "dialect": 
         {"delimiter": null,
-        "decimal_separator": "."} } ],
-"_comment": {
-    "_metadata_version": "1.4.0",
-    "_metadata_license": "Creative Commons Zero v1.0 Universal (CC0-1.0)",
-    "_metadata_license_url": "https://creativecommons.org/publicdomain/zero/1.0/",
-    "_additional_information": {
-        "_dates": "Dates and time must follow the ISO8601 including time zone (YYYY-MM-DD or YYYY-MM-DDThh:mm:ss±hh)",
-        "_units": "Use a space between Numbers and units (100 m)",
-        "_languages": "Languages must follow the IETF (BCP47) format (en-US; de-DE)",
-        "_null": "If not applicable use (null)"} } }';
+        "decimalSeparator": "."} } ],
+"review": {
+    "path": "https://github.com/OpenEnergyPlatform/data-preprocessing/wiki",
+    "badge": "platin"},
+"metaMetadata":
+    {"metadataVersion": "OEP-1.4.0",
+    "metadataLicense":
+        {"name": "CC0-1.0",
+        "title": "Creative Commons Zero v1.0 Universal",
+        "path": "https://creativecommons.org/publicdomain/zero/1.0/"} },
+"_comment":
+    {"metadata": "Metadata documentation and explanation (https://github.com/OpenEnergyPlatform/organisation/wiki/metadata)",
+    "dates": "Dates and time must follow the ISO8601 including time zone (YYYY-MM-DD or YYYY-MM-DDThh:mm:ss±hh)",
+    "units": "Use a space between numbers and units (100 m)",
+    "languages": "Languages must follow the IETF (BCP47) format (en-GB, en-US, de-DE)",
+    "licenses": "License name must follow the SPDX License List (https://spdx.org/licenses/)",
+    "review": "Following the OEP Data Review (https://github.com/OpenEnergyPlatform/data-preprocessing/wiki)",
+    "null": "If not applicable use (null)"} }';
 
 -- format verification
 SELECT obj_description('model_draft.oep_metadata_table_example_v14' ::regclass) ::json;
