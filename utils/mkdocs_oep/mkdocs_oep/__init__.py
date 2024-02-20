ed052e8d147231112de35f91fa59118f08f4bba7
c.winger <c.winger@oeko.de>
2024-02-20 14:18:18 +0100

    also replace logos in notebooks

--- utils/mkdocs_oep/mkdocs_oep/__init__.py
+++ utils/mkdocs_oep/mkdocs_oep/__init__.py
@@ -41,32 +41,34 @@ class OepPlugin(mkdocs.plugins.BasePlugin[OepPluginConfig]):
         # -----------------------------------------------------------------------
         # replace icons and logos shortcuts :oep-xxx-yyy:
         # -----------------------------------------------------------------------
-        path_prefix = get_relative_to_root(page.url)
-
         for key, val in set(re.findall(r":oep-([\w]+)-([\w]+):", markdown)):
             search = f":oep-{key}-{val}:"
             if key == "icon":
                 icon = self.config["icon"].get(val, val)
                 markdown = markdown.replace(search, f":{icon}:")
-            elif key == "logo":
-                logo = self.config["logo"].get(val, val)
-                markdown = markdown.replace(
-                    search, f'<img src="{path_prefix}{IMG_PATH}/{logo}" alt="logo">'
-                )
-            else:
-                raise NotImplementedError(f"Undefined prefix: oep-{key}")
 
         return markdown
 
     def on_page_content(self, html: str, page, config, files) -> str:
-        soup = bs4.BeautifulSoup(html)
-
-        import re
-
+        # -----------------------------------------------------------------------
         # if level 2 header starts with "About": mark this with "section-about" class
         # so that css can change formatting to small font
-        for item in soup.find_all("h2", attrs={"id": re.compile("about.*")}):
+        # -----------------------------------------------------------------------
+        soup = bs4.BeautifulSoup(html)
+        for item in soup.find_all("h2", attrs={"id": re.compile("^about.*")}):
             item["class"] = "section-about"
-
         html = str(soup)
+
+        # -----------------------------------------------------------------------
+        # replace icons and logos shortcuts :oep-xxx-yyy:
+        # -----------------------------------------------------------------------
+        path_prefix = get_relative_to_root(page.url)
+        for key, val in set(re.findall(r":oep-([\w]+)-([\w]+):", html)):
+            search = f":oep-{key}-{val}:"
+            if key == "logo":
+                logo = self.config["logo"].get(val, val)
+                html = html.replace(
+                    search, f'<img src="{path_prefix}{IMG_PATH}/{logo}" alt="logo">'
+                )
+
         return html
